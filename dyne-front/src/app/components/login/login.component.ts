091ee2514de6e55f9fc38144e9c033f171ae73bd
Manuel Kessel <liproqq@gmail.com>
2021-03-27 17:15:10 +0100

    add auth service

--- dyne-front/src/app/components/login/login.component.ts
+++ dyne-front/src/app/components/login/login.component.ts
@@ -1,13 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 import { Router } from '@angular/router';
-import { AuthService } from '../../services/auth-service.service'
+import { AuthService } from '../../services/auth.service'
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
+
 export class LoginComponent {
     form:FormGroup;
 
---
+++ dyne-front/src/app/services/auth.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient} from '@angular/common/http';
+import { pluck, share, shareReplay, tap } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  
+     
+    constructor(private http: HttpClient) {
+
+    }
+      
+    login(email:string, password:string ) {
+        return this.http.post('/api/login', {email, password})
+    }
+}
