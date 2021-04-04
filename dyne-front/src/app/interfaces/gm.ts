b9340ce2dbc82c0b58cbbb2e30cb6094073512a1
Manuel Kessel <liproqq@gmail.com>
2021-04-04 18:13:08 +0200

    login route frontend

---
+++ dyne-front/src/app/interfaces/gm.ts
@@ -0,0 +1,5 @@
+export interface IGm {
+    name: string;
+    password: string;
+    steam: string;
+}
--- dyne-front/src/app/services/auth.service.ts
+++ dyne-front/src/app/services/auth.service.ts
@@ -1,18 +1,49 @@
 import { Injectable } from '@angular/core';
-import { HttpClient} from '@angular/common/http';
-import { pluck, share, shareReplay, tap } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
+
+import * as moment from "moment";
+import { IGm } from '../interfaces/gm';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  
-     
-    constructor(private http: HttpClient) {
+  constructor(private http: HttpClient) {
 
-    }
-      
-    login(email:string, password:string ) {
-        return this.http.post('/api/login', {email, password})
-    }
+  }
+
+  login(name: string, password: string) {
+    return this.http.post<IGm>('/api/login', { name, password })
+      .pipe(
+        tap(res => this.setSession)
+      )
+
+  }
+
+  private setSession(authResult: { expiresIn: moment.DurationInputArg1; idToken: string; }) {
+    const expiresAt = moment().add(authResult.expiresIn, 'second');
+
+    localStorage.setItem('id_token', authResult.idToken);
+    localStorage.setItem("expires_at", JSON.stringify(expiresAt.valueOf()));
+  }
+
+  logout() {
+    localStorage.removeItem("id_token");
+    localStorage.removeItem("expires_at");
+  }
+
+  public isLoggedIn() {
+    return moment().isBefore(this.getExpiration());
+  }
+
+  isLoggedOut() {
+    return !this.isLoggedIn();
+  }
+
+  getExpiration() {
+    const expiration = localStorage.getItem("expires_at");
+    const expiresAt = JSON.parse(expiration || Date.now().toString());
+    return moment(expiresAt);
+  }
 }
