853ff10df6db72fd1d5bb54ea0729520b73e236b
Manuel Kessel <liproqq@gmail.com>
2021-04-10 15:31:02 +0200

    refactor material components to single file

--- dyne-front/src/app/app.module.ts
+++ dyne-front/src/app/app.module.ts
@@ -5,13 +5,8 @@ import { HttpClientModule } from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatMenuModule} from '@angular/material/menu';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatButtonModule} from '@angular/material/button';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {MatCardModule} from '@angular/material/card';
-
+import { MaterialModule } from './material/material.module';
 
 import { LoginComponent } from './components/login/login.component';
 import { MenuComponent } from './components/menu/menu.component';
@@ -28,14 +23,9 @@ import { EnterStatsComponent } from './components/enter-stats/enter-stats.compon
     AppRoutingModule,
     BrowserAnimationsModule,
     HttpClientModule,
-    MatMenuModule,
-    MatFormFieldModule,
-    MatCardModule,
     FormsModule,
     ReactiveFormsModule,
-    MatInputModule,
-    MatButtonModule
-    
+    MaterialModule    
   ],
   providers: [],
   bootstrap: [AppComponent]
